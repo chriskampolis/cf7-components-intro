bb3bddd3dc556529809c8c10d01ffd2c18e490e8
Chris Kampolis <chris.kampolis@outlook.com>
2025-05-28 11:12:52 +0300

    feat: create to-do app

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,8 @@ import Layout from "./components/Layout.tsx";
 // import CounterAdvanced from "./components/CounterAdvanced.tsx";
 // import CounterWithCustomHook from "./components/CounterWithCustomHook.tsx";
 // import CounterAdvancedWithCustomHook from "./components/CounterAdvancedWithCustomHook.tsx";
-import CounterWithReducer from "./components/CounterWithReducer.tsx";
+// import CounterWithReducer from "./components/CounterWithReducer.tsx";
+import Todo from "./components/Todo/Todo.tsx"
 
 function App() {
 
@@ -37,7 +38,8 @@ function App() {
         {/*<CounterWithCustomHook/>*/}
 
         {/*<CounterAdvancedWithCustomHook />*/}
-        <CounterWithReducer />
+        {/*<CounterWithReducer />*/}
+        <Todo/>
 
       </Layout>
     </>
