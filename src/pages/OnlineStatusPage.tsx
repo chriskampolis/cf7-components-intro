24349299d687969128e8793c35c75522bc5b954f
Chris Kampolis <chris.kampolis@outlook.com>
2025-06-04 19:33:25 +0300

    feat: add a page component for online status

---
+++ src/pages/OnlineStatusPage.tsx
@@ -0,0 +1,16 @@
+import OnlineStatus from "../components/OnlineStatus.tsx"
+import {useEffect} from "react";
+
+const OnlineStatusPage = () => {
+
+  useEffect(() => {
+    document.title = "CF7 Online Status";
+  }, [])
+
+  return (
+    <>
+      <OnlineStatus/>
+    </>
+  )
+}
+export default OnlineStatusPage;
