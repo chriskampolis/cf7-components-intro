ffa3521315d574ec59b5b2e4d68c46aca6e3d6f7
Chris Kampolis <chris.kampolis@outlook.com>
2025-06-04 19:32:59 +0300

    feat: add document titles

--- src/pages/NameChangerPage.tsx
+++ src/pages/NameChangerPage.tsx
@@ -1,6 +1,12 @@
 import NameChanger from "../components/NameChanger.tsx";
+import {useEffect} from "react";
 
 const NavigatorPage = () => {
+
+  useEffect(() => {
+    document.title = "CF7 Name Changer";
+  }, [])
+
   return (
     <>
       <NameChanger/>
